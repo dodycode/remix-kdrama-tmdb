a93d261db6eccd93b94c1e4d40dc39c40727b51a
Dody <prasetyodody17@gmail.com>
2024-03-13 05:57:09 +0700

    feat: filter by genre

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -7,7 +7,7 @@ import VStack from "~/components/vstack";
 import MovieListHeader from "./movie-list-header";
 import MovieList from "./movie-list";
 import { Await, useLoaderData } from "@remix-run/react";
-import { discoverTVShows } from "~/services/tmdb.server";
+import { discoverTVShows, getTVShowGenres } from "~/services/tmdb.server";
 import { Suspense, useEffect } from "react";
 import { useHydrated } from "remix-utils/use-hydrated";
 import MovieListHeaderSkeleton from "./movie-list-header-skeleton";
@@ -28,13 +28,17 @@ export async function loader({ request, context }: LoaderFunctionArgs) {
   const url = new URL(request.url);
   const sortBy = url.searchParams.get("sortBy") || "popularity.desc";
   const page = url.searchParams.get("page") || 1;
+  const withGenres = url.searchParams.get("with_genres") || "";
 
   const APIToken = context.env.THE_MOVIE_DB_ACCESS_TOKEN;
 
-  const kdramas = discoverTVShows(page as number, APIToken, sortBy);
+  const kdramas = discoverTVShows(page as number, APIToken, sortBy, withGenres);
+  const genres = getTVShowGenres(APIToken);
+
+  const data = Promise.all([kdramas, genres]);
 
   return defer({
-    kdramas,
+    data,
   });
 }
 
@@ -49,23 +53,26 @@ export default function Index() {
     <main className="w-full px-4 lg:px-0 lg:max-w-4xl mx-auto mt-4 lg:mt-10">
       <VStack className="gap-y-5 h-[calc(100vh-105px)]">
         <Suspense fallback={<MovieListHeaderSkeleton />}>
-          <Await resolve={data.kdramas}>
-            {(kdramas) => {
+          <Await resolve={data.data}>
+            {([_, genres]: any) => {
               //@ts-ignore
-              if (!kdramas.results) return <MovieListHeaderSkeleton />;
-              return <MovieListHeader />;
+              if (!genres.genres) return <MovieListHeaderSkeleton />;
+              return <MovieListHeader genres={genres.genres} />;
             }}
           </Await>
         </Suspense>
 
         <VStack className="flex-1">
           <Suspense fallback={<MovieListSkeleton />}>
-            <Await resolve={data.kdramas}>
-              {(kdramas) => {
+            <Await resolve={data.data}>
+              {([kdramas, genres]: any) => {
                 //@ts-ignore
-                if (!kdramas.results) return <div>No data</div>;
+                if (!kdramas.results.length) return <div>No results</div>;
+
                 //@ts-ignore
-                return <MovieList movies={kdramas.results} />;
+                return (
+                  <MovieList genres={genres.genres} movies={kdramas.results} />
+                );
               }}
             </Await>
           </Suspense>
